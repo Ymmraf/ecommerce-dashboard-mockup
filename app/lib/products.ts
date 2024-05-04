37a3df3e60a3e748e583bc6738d61ce5df50af5b
Pheranat Lorwongkamol <Pheranatlor@gmail.com>
2024-05-04 14:41:29 +0700

    add swiper and discount goods

---
+++ app/lib/products.ts
@@ -0,0 +1,7 @@
+export const products = [
+    {name: "Orange", type: "fresh", price: 3, stock: 20, discount: 0.1, img: "/fruits/orange.jpg", desc: "An orange", rating: 4.9},
+    {name: "Apple", type: "fresh", price: 3, stock: 20, discount: 0.1, img: "/fruits/apple.jpg", desc: "An apple", rating: 4.8},
+    {name: "Giwi", type: "fresh", price: 5, stock: 20, discount: 0, img: "/fruits/giwi.jpg", desc: "A giwi", rating: 5},
+    {name: "Mango", type: "fresh", price: 4, stock: 20, discount: 0, img: "/fruits/mango.jpg", desc: "A mango", rating: 5},
+    // {name: "Strawberry", type: "fresh", price: 6, stock: 20, discount: 0, img: "/fruits/strawberry.jpg", desc: "A strawberry", rating: 4.9},
+]

418cbc451ce4bfca8892e7a92e51199f9c729a69
Pheranat Lorwongkamol <Pheranatlor@gmail.com>
2024-05-05 23:02:08 +0700

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+export default function NotFound() {
+  return (
+    <main className="h-[83vh] flex justify-center items-center">
+      <div className="w-4/5 m-auto">
+        <div className="size-20 m-auto mb-8 text-tomato">
+          <FontAwesomeIcon icon={faCircleXmark} className="size-20"/>
+        </div>
+        <p className="text-center text-coal font-bold text-4xl mb-2">Whoops!</p>
+        <h1 className="text-center text-coal text-2xl font-light">This page cound not be found.</h1>
+        <div className="flex justify-center">
+          <Link href="/" className="text-center w-48 p-3 bg-leaf text-cream font-semibold rounded-xl mt-8 hover:scale-105 duration-300">Back to homepage</Link>
+        </div>
+      </div>
+    </main>
+  );
+}

c057153ecfd4cdc8afc7c9c6b3a80300bbde4960
Pheranat Lorwongkamol <Pheranatlor@gmail.com>
2024-05-09 01:00:12 +0700

    shorten setfilter function

--- app/[store]/page.tsx
+++ app/[store]/page.tsx
@@ -1,30 +1,111 @@
 "use client";
 
 import Footer from "../ui/Footer";
-import { Suspense } from "react";
 import Breadcrumbs from "../ui/Breadcrumbs";
 import ProductCard from "../ui/ProductCard";
+import Filter from "../ui/store/Filter";
+import { Suspense } from "react";
 import { getDecimal } from "../utils/getDecimal";
 import { useState, useEffect } from "react";
 import { ProductCardSkeleton } from "../ui/Skeleton";
-import Filter from "../ui/store/Filter";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 
 export default function Store() {
-  const [data, setData] = useState<any[]>([]);
-  const [display, setDisplay] = useState<any[]>([]);
-  const [filter, setFilter] = useState<any[]>([]);
+  const [initialData, setinitialData] = useState<any>("");
   const [sideFilter, setSideFilter] = useState(false);
+  const [display, setDisplay] = useState<any[]>([]);
+  const [currentFilter, setCurrentFilter] = useState<any>({
+    type: "",
+    state: "",
+    price: ""
+  });
 
-  const [type, setType] = useState("");
-  const [state, setState] = useState("");
-  const [price, setPrice] = useState("");
-  const [name, setName] = useState("");
-  
+  const [typeFilter, setTypeFilter] = useState<any>("");
+  const [stateFilter, setStateFilter] = useState<any>("");
+  const [priceSort, setPriceSort] = useState<any>("");
+
+  interface FilterObject {
+    filter: string 
+    value: string
+  }
+
+  function handleClickFilter(filter : FilterObject) {
+    if(!currentFilter[filter.filter]) {
+      setCurrentFilter({
+        ...currentFilter,
+        [filter.filter] : filter.value
+      })
+    } else if (currentFilter[filter.filter]) {
+      if(currentFilter[filter.filter] == filter.value) {
+        setCurrentFilter({
+          ...currentFilter,
+          [filter.filter] : ""
+        })
+      } else if (currentFilter[filter.filter] != filter.value) {
+        setCurrentFilter({
+          ...currentFilter,
+          [filter.filter] : filter.value
+        })
+      }
+    }
+  }
+
+  function handleClickType(typeStr: string) {
+    if (!typeFilter) {
+      setTypeFilter(typeStr);
+      setCurrentFilter({...currentFilter, type: typeStr})
+    } else if (typeFilter) {
+      if (typeFilter == typeStr) {
+        setTypeFilter("");
+        setCurrentFilter({...currentFilter, type: ""})
+      } else if (typeFilter != typeStr) {
+        setTypeFilter(typeStr);
+        setCurrentFilter({...currentFilter, type: typeStr})
+      }
+    }
+  }
+
+  function handleClickState(stateStr: string) {
+    if (!stateFilter) {
+      setStateFilter(stateStr);
+      setCurrentFilter({...currentFilter, state: stateStr})
+    } else if (stateFilter) {
+      if (stateFilter == stateStr) {
+        setStateFilter("");
+        setCurrentFilter({...currentFilter, state: ""})
+      } else if (stateFilter != stateStr) {
+        setStateFilter(stateStr);
+        setCurrentFilter({...currentFilter, state: stateStr})
+      }
+    }
+  }
+
+  function handleClickPrice(priceStr: string) {
+    if (!priceSort) {
+      setPriceSort(priceStr);
+      setCurrentFilter({...currentFilter, price: priceStr})
+    } else if (priceSort) {
+      if (priceSort == priceStr) {
+        setPriceSort("");
+        setCurrentFilter({...currentFilter, price: ""})
+      } else if (priceSort != priceStr) {
+        setPriceSort(priceStr);
+        setCurrentFilter({...currentFilter, price: priceStr})
+      }
+    }
+  }
+
   useEffect(() => {
-    fetchAllProducts();
-  }, []);
+    if(!initialData) {
+      fetchAllProducts();
+    }
+
+  }, [currentFilter]);
+
+  function filterAndSort() {
+    
+  }
 
   function toggleSideFilter() {
     setSideFilter(() => !sideFilter);
@@ -33,7 +114,7 @@ export default function Store() {
   async function fetchAllProducts() {
     const allProduct = await fetch("/store/api");
     const json = await allProduct.json();
-    setData(json.res.rows);
+    setinitialData(json.res.rows);
     setDisplay(json.res.rows);
   }
 
@@ -47,13 +128,25 @@ export default function Store() {
           className="text-coal px-4 py-2 text-xl w-full text-left hover:bg-darkcream flex justify-between my-8 rounded-xl duration-300"
         >
           <div className="font-semibold opacity-90">
-            Filter ({filter.length}) <FontAwesomeIcon className="size-4" icon={faFilter}/>
+            Filter ({currentFilter.length}){" "}
+            <FontAwesomeIcon className="size-4" icon={faFilter} />
           </div>
           <div className="font-regular">
             Show {display.length} {display.length > 1 ? "goods" : "good"}
           </div>
         </button>
-        <Filter state={sideFilter} toggle={toggleSideFilter} />
+        <Filter
+          state={sideFilter}
+          toggle={toggleSideFilter}
+          typeFilter={typeFilter}
+          handleClickType={handleClickType}
+          stateFilter={stateFilter}
+          handleClickState={handleClickState}
+          priceSort={priceSort}
+          handleClickPrice={handleClickPrice}
+          // nameSort={nameSort}
+          // handleClickName={handleClickName}
+        />
         <section className="grid grid-cols-2 gap-x-2 gap-y-6 mt-8">
           <Suspense fallback={<ProductCardSkeleton />}>
             {display.map((product, index) => (
--- app/ui/store/Filter.tsx
+++ app/ui/store/Filter.tsx
@@ -2,51 +2,140 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import clsx from "clsx";
 
-export default function Filter({state, toggle} : {state: boolean, toggle: Function}) {
-    return (
-        <aside className={clsx("fixed z-40 left-0 top-0 w-4/5 h-full border-1 bg-cream border-coal border-px shadow-md",
-            {
-                "block" : state,
-                "hidden" : !state
-            }
-        )}
+export default function Filter({
+  state,
+  toggle,
+  typeFilter,
+  handleClickType,
+  stateFilter,
+  handleClickState,
+  priceSort,
+  handleClickPrice,
+}: {
+  state: boolean;
+  toggle: Function;
+  typeFilter: string;
+  handleClickType: Function;
+  stateFilter: string;
+  handleClickState: Function;
+  priceSort: string;
+  handleClickPrice: Function;
+}) {
+  return (
+    <aside
+      className={clsx(
+        "fixed z-40 left-0 top-0 w-4/5 h-full border-1 bg-cream border-coal border-px shadow-md",
+        {
+          block: state,
+          hidden: !state,
+        }
+      )}
+    >
+      <div className="pt-[61px]">
+        <button
+          onClick={() => toggle()}
+          className="p-1 m-1 hover:bg-darkcream rounded-xl duration-300"
         >
-            <div className="pt-[61px]">
-                <button onClick={() => toggle()} className="p-1 m-1 hover:bg-darkcream rounded-xl duration-300"><FontAwesomeIcon className="size-8 text-coal" icon={faXmark} /></button>
-            </div>
-            <div className="p-4">
-                <div>
-                    <p className="text-coal text-lg font-semibold mb-3">Type</p>
-                    <ul className="flex gap-x-2">
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">Fresh fruit</button></li>
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">Dried fruit</button></li>
-                    </ul>
-                </div>
-                <hr className="h-[2px] w-full bg-darkcream my-4"/>
-                <div>
-                    <p className="text-coal text-lg font-semibold mb-3">State</p>
-                    <ul className="flex gap-x-2">
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">New addition</button></li>
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">Discount</button></li>
-                    </ul>
-                </div>
-                <hr className="h-[2px] w-full bg-darkcream my-4"/>
-                <div>
-                    <p className="text-coal text-lg font-semibold mb-3">Sort by price</p>
-                    <ul className="flex gap-x-2">
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">High to low</button></li>
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">Low to High</button></li>
-                    </ul>
-                </div>
-                <hr className="h-[2px] w-full bg-darkcream my-4"/>
-                <div>
-                    <p className="text-coal text-lg font-semibold mb-3">Sort by name</p>
-                    <ul className="flex gap-x-2 mb-6">
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">A - Z</button></li>
-                        <li><button className="px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300">Z - A</button></li>
-                    </ul>
-                </div>
-            </div>
-        </aside>
-    )
+          <FontAwesomeIcon className="size-8 text-coal" icon={faXmark} />
+        </button>
+      </div>
+      <div className="p-4">
+        <div>
+          <p className="text-coal text-lg font-semibold mb-3">Type</p>
+          <ul className="flex gap-x-2">
+            <li>
+              <button
+                onClick={() => handleClickType("fresh")}
+                className={clsx(
+                  "px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300",
+                  {
+                    "bg-coal text-cream": typeFilter == "fresh",
+                  }
+                )}
+              >
+                Fresh fruit
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => handleClickType("dried")}
+                className={clsx(
+                  "px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300",
+                  {
+                    "bg-coal text-cream": typeFilter == "dried",
+                  }
+                )}
+              >
+                Dried fruit
+              </button>
+            </li>
+          </ul>
+        </div>
+        <hr className="h-[2px] w-full bg-darkcream my-4" />
+        <div>
+          <p className="text-coal text-lg font-semibold mb-3">State</p>
+          <ul className="flex gap-x-2">
+            <li>
+              <button
+                onClick={() => handleClickState("new")}
+                className={clsx(
+                  "px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300",
+                  {
+                    "bg-coal text-cream": stateFilter == "new",
+                  }
+                )}
+              >
+                New addition
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => handleClickState("discount")}
+                className={clsx(
+                  "px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300",
+                  {
+                    "bg-coal text-cream": stateFilter == "discount",
+                  }
+                )}
+              >
+                Discount
+              </button>
+            </li>
+          </ul>
+        </div>
+        <hr className="h-[2px] w-full bg-darkcream my-4" />
+        <div>
+          <p className="text-coal text-lg font-semibold mb-3">Sort by price</p>
+          <ul className="flex gap-x-2">
+            <li>
+              <button
+                onClick={() => handleClickPrice("desc")}
+                className={clsx(
+                  "px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300",
+                  {
+                    "bg-coal text-cream": priceSort == "desc",
+                  }
+                )}
+              >
+                High to low
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => handleClickPrice("asc")}
+                className={clsx(
+                  "px-4 py-2 text-coal border-[1px] rounded-xl border-coal hover:text-cream hover:bg-coal duration-300",
+                  {
+                    "bg-coal text-cream": priceSort == "asc",
+                  }
+                )}
+              >
+                Low to High
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </aside>
+  );
 }

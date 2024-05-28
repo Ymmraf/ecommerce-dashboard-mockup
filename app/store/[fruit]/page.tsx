c38af97cfaa04f49d3b48f22a502a72b1e8a5a09
Pheranat Lorwongkamol <Pheranatlor@gmail.com>
2024-05-28 14:37:03 +0700

    add customer review component

--- app/store/[fruit]/page.tsx
+++ app/store/[fruit]/page.tsx
@@ -9,6 +9,7 @@ import AccordionFruit from "@/app/ui/fruit/Accordion";
 import clsx from "clsx";
 import Footer from "@/app/ui/Footer";
 import Order from "@/app/ui/fruit/Order";
+import CustomerReview from "@/app/ui/fruit/CustomerReview";
 
 export default async function Fruit({ params }: { params: { fruit: string } }) {
   const fruitName = capitalizeParams(params.fruit);
@@ -24,53 +25,60 @@ export default async function Fruit({ params }: { params: { fruit: string } }) {
         >
           &lt; To all product
         </Link>
-        <h1 className="text-coal text-2xl font-semibold mt-4">{fruitName}</h1>
-        <Suspense fallback={<ProductCardSkeleton />}>
-          <div className="w-3/5 shadow-lg rounded-full my-12 mx-auto">
-            <Image
-              className="rounded-full"
-              src={productInfo.img}
-              alt={productInfo.name}
-              width={500}
-              height={500}
+        <section>
+          <h1 className="text-coal text-2xl font-semibold mt-4">{fruitName}</h1>
+          <Suspense fallback={<ProductCardSkeleton />}>
+            <div className="w-3/5 shadow-lg rounded-full my-12 mx-auto">
+              <Image
+                className="rounded-full"
+                src={productInfo.img}
+                alt={productInfo.name}
+                width={500}
+                height={500}
+              />
+            </div>
+          </Suspense>
+        </section>
+        <section>
+          <div className="flex justify-between">
+            <div
+              className={clsx(
+                "font-semibold px-4 py-1 border-2 rounded-lg border-coal border-opacity-50",
+                {
+                  "text-leaf": productInfo.stock,
+                  "text-tomato": !productInfo.stock,
+                }
+              )}
+            >
+              {productInfo.stock ? "In stock ✓" : "Out of stock X"}
+            </div>
+            <div className="flex items-center">
+              <p className="text-coal opacity-70">Delivery: 1-3 business days</p>
+            </div>
+          </div>
+          <div>
+            {
+              productInfo.stock <= 3 && productInfo.stock > 0 ? 
+              <p className="text-tomato font-semibold text-sm pl-3">{productInfo.stock} remaining</p> :
+              <p></p>
+            }
+          </div>
+        </section>
+        <section>
+          <article className="my-8">
+            <h2 className="text-coal text-xl">Description:</h2>
+            <p className="text-coal mt-4 tracking-wide leading-7">
+              {productInfo.detail}
+            </p>
+          </article>
+          <div>
+            <AccordionFruit
+              nutrition={productInfo.nutrition}
+              origin={productInfo.origin}
             />
           </div>
-        </Suspense>
-        <div className="flex justify-between">
-          <div
-            className={clsx(
-              "font-semibold px-4 py-1 border-2 rounded-lg border-coal border-opacity-50",
-              {
-                "text-leaf": productInfo.stock,
-                "text-tomato": !productInfo.stock,
-              }
-            )}
-          >
-            {productInfo.stock ? "In stock ✓" : "Out of stock X"}
-          </div>
-          <div className="flex items-center">
-            <p className="text-coal opacity-70">Delivery: 1-3 business days</p>
-          </div>
-        </div>
-        <div>
-          {
-            productInfo.stock <= 3 && productInfo.stock > 0 ? 
-            <p className="text-tomato font-semibold text-sm pl-3">{productInfo.stock} remaining</p> :
-            <p></p>
-          }
-        </div>
-        <article className="my-8">
-          <h2 className="text-coal text-xl">Description:</h2>
-          <p className="text-coal mt-4 tracking-wide leading-7">
-            {productInfo.detail}
-          </p>
-        </article>
-        <div>
-          <AccordionFruit
-            nutrition={productInfo.nutrition}
-            origin={productInfo.origin}
-          />
-        </div>
+        </section>
+        
       </main>
       <Order
         price={(productInfo.price * (1 - productInfo.discount)).toFixed(2)}
@@ -78,6 +86,9 @@ export default async function Fruit({ params }: { params: { fruit: string } }) {
         img={productInfo.img}
         stock={productInfo.stock}
       />
+      <aside className="w-11/12 m-auto">
+          <CustomerReview rating={productInfo.rating}/>
+      </aside>
       <Footer />
     </>
   );
---
+++ app/ui/fruit/CustomerReview.tsx
@@ -0,0 +1,73 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar } from "@fortawesome/free-solid-svg-icons"
+
+export default function CustomerReview({rating} : {rating : number}) {
+    const reviewArr = [
+        {
+            name: "Kunanon",
+            review: "The fruit is so fresh! I really love the texture and taste!",
+            rating: 4,
+            date: "May 10, 2024"
+        },
+        {
+            name: "Pheranat",
+            review: "I really love it! The texture, freshness, flavour and aroma. This is perfect!",
+            rating: 5,
+            date: "April 30, 2024"
+        },
+        {
+            name: "Danaisate",
+            review: "Mmmm... pretty good",
+            rating: 3,
+            date: "March 15, 2024"
+        },
+
+    ]
+
+    return (
+        <>
+            <div className="flex justify-between text-coal mt-8 mb-4">
+                <h2 className="text-xl">Customer review</h2>
+                <div className="flex gap-x-2">
+                    <FontAwesomeIcon className="relative top-[3px] text-leaf" icon={faStar}/>
+                    <p className="text-leaf font-semibold">{rating} / 5</p>
+                    <p className="opacity-70">(3 reviews)</p>
+                </div>
+            </div>
+            <div className="space-y-4 text-coal">
+                {
+                    reviewArr.map((review, index) => 
+                        <ReviewBlock key={index} name={review.name} review={review.review} rating={review.rating} date={review.date}/>
+                    )
+                }
+            </div>
+        </>
+    )
+}
+
+function ReviewBlock({name, review, rating, date} : {name : string, review : string, rating : number, date: string}) {
+    const ratingArr = [1,1,1,1,1]
+
+    return (
+      <div className="bg-white rounded-xl p-4">
+        <div className="space-y-4">
+          <h3 className="font-semibold text-xl">{name}</h3>
+          <p>{review}</p>
+          <div className="flex justify-between">
+            <p>{date}</p>
+            <div>
+                {
+                    ratingArr.map((element, index) => {
+                        if(index+1 > Math.floor(rating)) {
+                            return <FontAwesomeIcon key={index} icon={faStar} className="text-coal opacity-50"/>
+                        } else {
+                            return <FontAwesomeIcon key={index} icon={faStar} className="text-leaf"/>
+                        }
+                    })
+                }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}

a8292e56c4c91e6ce6225deeace7e906a89adb0b
Pheranat Lorwongkamol <Pheranatlor@gmail.com>
2024-06-26 17:06:42 +0700

    fixed submit change

--- app/dashboard/product/[fruitname]/submitChange.ts
+++ app/dashboard/product/[fruitname]/submitChange.ts
@@ -1,28 +1,28 @@
-'use server'
+// 'use server'
 
-import { sql } from "@vercel/postgres"
+// import { sql } from "@vercel/postgres"
 
-async function submit(formData : FormData) {
-    'use server'
-    const rawFormData = {
-        add: formData.get('add'),
-        stock: formData.get('stock'),
-        price: formData.get('price'),
-        discount: formData.get('discount'),
-        detail: formData.get('detail'),
-        origin: formData.get('origin'),
-        nutrition: formData.get('nutrition')
-    }
+// async function submit(formData : FormData) {
+//     'use server'
+//     const rawFormData = {
+//         add: formData.get('add'),
+//         stock: formData.get('stock'),
+//         price: formData.get('price'),
+//         discount: formData.get('discount'),
+//         detail: formData.get('detail'),
+//         origin: formData.get('origin'),
+//         nutrition: formData.get('nutrition')
+//     }
 
-    try {
-        await sql `
-            UPDATE product
-            SET stock = ${Number(rawFormData.stock)}
-            WHERE name = ${fruitName};
-        `
-    } catch (error) {
-        console.log(`Database error : ${error}`)
-        throw new Error("Failed to update product data")
-    }
-    console.log(rawFormData)
-}
+//     try {
+//         await sql `
+//             UPDATE product
+//             SET stock = ${Number(rawFormData.stock)}
+//             WHERE name = ${fruitName};
+//         `
+//     } catch (error) {
+//         console.log(`Database error : ${error}`)
+//         throw new Error("Failed to update product data")
+//     }
+//     console.log(rawFormData)
+// }
